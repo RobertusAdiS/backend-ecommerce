2080fc7af4b3639905a24400a1ab65db1add73be
RobertusAdiS <robertusadi.2020@student.uny.ac.id>
2024-12-06 13:38:39 +0700

    make Restful API Dashboard Admin

--- routes/api.php
+++ routes/api.php
@@ -28,5 +28,7 @@ Route::prefix('admin')->group(function () {
         Route::get('/refresh',[App\Http\Controllers\Api\Admin\LoginController::class, 'refreshToken',['as' => 'admin']]);
         //logout
         Route::post('/logout',[App\Http\Controllers\Api\Admin\LoginController::class, 'logout', ['as'=> 'admin']]);
+        //dashboard
+        Route::get('/dashboard',[App\Http\Controllers\Api\Admin\DashboardController::class, 'index', ['as'=> 'admin']]);
     });
 });
